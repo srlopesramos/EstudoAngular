59ae1b8be84124aa2692bb2b9e69cf81804ccb60
Odair L R Jr <srlopesramos2.0@gmail.com>
2025-05-05 16:51:34 -0300

    feat: Unitary test to calculadora.component

--- src/app/calculadora/components/calculadora.component.spec.ts
+++ src/app/calculadora/components/calculadora.component.spec.ts
@@ -1,7 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CalculadoraComponent } from './calculadora.component';
+import {CalculadoraComponent} from './calculadora.component';
 import {CalculadoraService} from '../services';
+import {By} from '@angular/platform-browser';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -12,7 +13,7 @@ describe('CalculadoraComponent', () => {
       imports: [CalculadoraComponent],
       providers: [CalculadoraService],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(CalculadoraComponent);
     component = fixture.componentInstance;
@@ -22,4 +23,27 @@ describe('CalculadoraComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be guaranteed that 3 + 2 = 5', () => {
+    const btn2 = fixture.debugElement.query(By.css('#btn2'));
+    const btn3 = fixture.debugElement.query(By.css('#btn3'));
+    const btnSoma = fixture.debugElement.query(By.css('#btnSoma'));
+    const btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'));
+    const display = fixture.debugElement.query(By.css('#display'));
+
+    btn3.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnSoma.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnCalcular.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('5');
+  });
+
 });
