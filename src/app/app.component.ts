40e67daefeafffde3c6aa14a89d3031f05c00acb
Odair L R Jr <srlopesramos2.0@gmail.com>
2025-05-05 19:56:54 -0300

    feat: Creada Moule e Model de Tarefas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import { TarefasModule } from './modules'
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    TarefasModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app-routing.module';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay())
+  ],
 };
