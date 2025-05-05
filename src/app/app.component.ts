4dc222924c5c2fab75c5bbfcded7e3b1f7eaac9a
Odair L R Jr <srlopesramos2.0@gmail.com>
2025-05-05 19:11:24 -0300

    feat: base to start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'gerenciador-de-tarefas';
+  title = 'Gerenciador de tarefas';
 }
