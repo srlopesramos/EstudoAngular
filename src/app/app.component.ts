070478b7f22393a6625a7df8b04b054cbd716956
Odair L R Jr <srlopesramos2.0@gmail.com>
2025-05-07 11:48:35 -0300

    feat: Creadas as tarefas para o projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import { TarefasModule } from './modules'
+import { TarefasModule } from './tarefas'
 
 @Component({
   selector: 'app-root',
