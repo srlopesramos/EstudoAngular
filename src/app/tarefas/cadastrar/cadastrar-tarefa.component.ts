bba73e5a62a9b756f5519c2acdb4d0c131d11900
Odair L R Jr <srlopesramos2.0@gmail.com>
2025-05-08 21:19:59 -0300

    feat: Cadastro de tarefas

--- src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
+++ src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
+import {FormsModule, NgForm} from '@angular/forms';
+import {NgIf} from '@angular/common';
+
+import { TarefaService, Tarefa } from '../shared';
+
+
+
 
 @Component({
   selector: 'app-cadastrar-tarefa',
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule,
+    NgIf
   ],
   templateUrl: './cadastrar-tarefa.component.html',
   styleUrl: './cadastrar-tarefa.component.scss'
 })
-export class CadastrarTarefaComponent {
+export class CadastrarTarefaComponent implements  OnInit {
 
-  protected readonly history = history;
+  @ViewChild('formTarefa', { static: true }) formTarefa: NgForm;
+  tarefa: Tarefa;
+
+  constructor(
+    private tarefaService: TarefaService,
+    private router: Router){}
+
+  ngOnInit() {
+    this.tarefa = new Tarefa();
+  }
+
+  cadastrar(): void{
+    if(this.formTarefa.form.valid){
+      this.tarefaService.cadastrar(this.tarefa);
+      this.router.navigate(["/tarefas"]);
+    }
+  }
+
 }
