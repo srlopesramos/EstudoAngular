87082ae118fad92eaf5d6c7ac3166e87d177c62c
Odair L R Jr <srlopesramos2.0@gmail.com>
2025-05-12 09:57:17 -0300

    feat: Remover Tarefa

--- src/app/tarefas/listar/listar-tarefas.component.ts
+++ src/app/tarefas/listar/listar-tarefas.component.ts
@@ -24,4 +24,19 @@ export class ListarTarefasComponent implements OnInit {
     return this.tarefaService.listarTodos();
   }
 
+  remover($event: any, tarefa: Tarefa):void{
+    $event.preventDefault();
+    if(confirm('Deseja remover a tarefa "'+ tarefa.nome +'"?')){
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
+  }
+
+  alertarStatus(tarefa: Tarefa):void {
+    if(confirm('Deseja alterar o status da tarefa "'+ tarefa.nome +'"?')){
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
+  }
+
 }
